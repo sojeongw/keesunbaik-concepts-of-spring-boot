6ed98124b783e45ee4de1821f9e719a40fb672de
sojeongw <sojeongw.official@gmail.com>
2022-08-17 06:31:52 +0900

    first commit

---
+++ src/main/java/me/whiteship/KeesunbaikConceptsOfSpringBootApplication.java
@@ -0,0 +1,13 @@
+package me.whiteship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeesunbaikConceptsOfSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeesunbaikConceptsOfSpringBootApplication.class, args);
+    }
+
+}
