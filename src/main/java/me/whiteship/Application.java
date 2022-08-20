a0ab088c84304e091a80af4d8fba1148526ecceb
sojeongw <sojeongw.official@gmail.com>
2022-08-20 10:56:02 +0900

    스프링 부트 개념과 활용: HTTPS와 HTTP2

--- src/main/java/me/whiteship/Application.java
+++ src/main/java/me/whiteship/Application.java
@@ -2,11 +2,20 @@ package me.whiteship;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class Application {
+
+    @GetMapping("hello")
+    public String hello() {
+        return "hello spring";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
 }
