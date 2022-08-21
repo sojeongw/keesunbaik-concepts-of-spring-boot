466a0c2d76906e759efb4a875fd612774e58c189
sojeongw <sojeongw.official@gmail.com>
2022-08-21 20:04:06 +0900

    스프링 부트 개념과 활용: SpringApplication

--- src/main/java/me/whiteship/Application.java
+++ src/main/java/me/whiteship/Application.java
@@ -2,19 +2,13 @@ package me.whiteship;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @SpringBootApplication
 public class Application {
-
-    @GetMapping("hello")
-    public String hello() {
-        return "hello spring";
-    }
-
     public static void main(String[] args) {
+//        SpringApplication app = new SpringApplication(Application.class);
+//        app.setWebApplicationType(WebApplicationType.NONE);
+//        app.run(args);
         SpringApplication.run(Application.class, args);
     }
 
