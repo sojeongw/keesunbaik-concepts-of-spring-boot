a2481f397e91b9d89b0b4ab35a5ff82a43e84b5e
sojeongw <sojeongw.official@gmail.com>
2022-08-26 06:44:03 +0900

    스프링 부트 개념과 활용: 스프링 웹 MVC - 소개

---
+++ src/test/java/me/whiteship/UserControllerTest.java
@@ -0,0 +1,24 @@
+package me.whiteship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(UserController.class)
+public class UserControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void hello() throws Exception {
+        mockMvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("hello"));
+    }
+}
