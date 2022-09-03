19b69810b02fb55483b09a58d8f86860b6ab9055
sojeongw <sojeongw.official@gmail.com>
2022-09-03 19:48:55 +0900

    스프링 부트 개념과 활용: CORS

--- src/main/java/me/whiteship/Application.java
+++ src/main/java/me/whiteship/Application.java
@@ -2,13 +2,21 @@ package me.whiteship;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Application {
+
+    @CrossOrigin(origins = "http://localhost:18080")
+    @GetMapping("/cross-origin")
+    public String crossOrigin() {
+        return "crossOrigin";
+    }
+
     public static void main(String[] args) {
-//        SpringApplication app = new SpringApplication(Application.class);
-//        app.setWebApplicationType(WebApplicationType.NONE);
-//        app.run(args);
         SpringApplication.run(Application.class, args);
     }
 
